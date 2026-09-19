ed41bf41767abc66a9d8796a823ccfbdcdcbcf80
Francisco Schneedorf <franciscoslf7@gmail.com>
2026-09-19 10:33:30 -0300

    Atualiza as funções com exceção de progredir

--- maze.c
+++ maze.c
@@ -1,77 +1,106 @@
 #include <stdio.h>
-#include <string.h>
 
-
-int leMatriz(FILE* arch, char maze[10][10])
+int leMatriz(FILE *arch, char maze[10][10], int *linhaEntrada, int *colunaEntrada)
 {
+    int encontrouEntrada = 0;
     // Laco para percorrer os elementos da matriz
-    for(int i = 0; i < 10; i++){ 
-        for(int j = 0; j < 10; j++){
-            if((fscanf(arch, " %c", &maze[i][j]) != 1)){ // Le os elementos da matriz controlada por arch e armazena em ordem
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if ((fscanf(arch, " %c", &maze[i][j]) == 1)) // Se fscanf obteve sucesso na execucao
+            {
+                if (maze[i][j] == 'E') // Verifica se o caractere naquela posicao e a entrada
+                {
+                    *linhaEntrada = i;
+                    *colunaEntrada = j;
+                    encontrouEntrada = 1;
+                }
+            }
+            else
+            {
                 printf("Falha na leitura.");
-                return 1; 
-            } 
+                return 1;
+            }
         }
+    }
+    if (encontrouEntrada == 0) // Caso nao encontre a entrada
+    {
+        printf("O labirinto nao possui entrada.");
+        return 1;
     }
     printf("Leitura realizada com sucesso!");
     return 0;
 }
 
-
-int abreMatriz (char maze[10][10], const char name[]){
-    FILE* arch; // Ponteiro que recebera o controle do arquivo aberto
-    if((arch = fopen(name, "r")) != NULL) // Verifica se o arquivo foi aberto corretamente
+int abreMatriz(char maze[10][10], const char name[], int *h, int *l)
+{
+    FILE *arch; // Ponteiro que recebera o controle do arquivo aberto
+    if ((arch = fopen(name, "r")) != NULL) // Verifica se o arquivo foi aberto corretamente
     {
         int result;
         printf("Labirinto aberto com sucesso!\nRealizando a leitura...");
-        result = leMatriz(arch, maze); // Chama a funcao para realizar a leitura
+        result = leMatriz(arch, maze, h, l); // Chama a funcao para realizar a leitura
         fclose(arch);
         return result;
-    } else {
+    }
+    else
+    {
         printf("Nao foi possivel abrir o labirinto. Tente novamente.");
         return 1;
     }
 }
 
-int progredir (char maze[10][10], int h, int l){
+int progredir(char maze[10][10], int h, int l)
+{
 
-if (maze[h][l] != '0' && 'E'){
-    return 0;
-} 
-if (maze[h][l] == 'S'){
-    return 1;
-} 
+    if (maze[h][l] != '0' && 'E')
+    {
+        return 0;
+    }
+    if (maze[h][l] == 'S')
+    {
+        return 1;
+    }
 
-maze[h][l] = '1';
+    maze[h][l] = '1';
 
-if (progredir(maze, h-1, l)){
-    return 1;
+    if (progredir(maze, h - 1, l))
+    {
+        return 1;
+    }
+
+    if (progredir(maze, h, l + 1))
+    {
+        return 1;
+    }
+
+    if (progredir(maze, h + 1, l))
+    {
+        return 1;
+    }
+
+    if (progredir(maze, h, l - 1))
+    {
+        return 1;
+    }
 }
 
-if (progredir(maze, h, l+1)){
-    return 1;
-}
-
-if (progredir(maze, h+1, l)){
-    return 1;
-}
-
-if (progredir(maze, h, l-1)){
-    return 1;
-}
-
-}
-
-int main(){
+int main(void)
+{
     // Variaveis
     char nome[100];
     char maze[10][10];
     int resultado;
+    int h, l;
     // Entrada de dados
     printf("Insira o nome do arquivo a ser aberto (inclua .txt):\n");
     scanf("%99s", nome);
     // Chama a funcao e verifica o return
-    if((resultado = abreMatriz(maze, nome)) != 0){
+    if ((resultado = abreMatriz(maze, nome, &h, &l)) != 0)
+    { // Se for == 0, maze esta preenchida e pronta para ser entregue a funcao progredir
         return 1;
-    } 
+    }
+
+    progredir(maze, h, l);
 }
